bed5b70e37c1036783e74c9a0d6210ab9381dc62
BrysonHinsch <brysonhinsch@gmail.com>
2026-06-30 15:49:56 -0400

    finish main functionality

--- content_scripts/socialblock.js
+++ content_scripts/socialblock.js
@@ -1,11 +1,32 @@
 
+// Main check timestamps
 let last_time_checked = Date.now();
+let last_day = new Date().toString();
+
+// popup + button timestamps
 let ignore = false;
+let popup_visible = false;
+let limit_extension = 0; // Will always go off the first time
 
 function update_limit_used() {
+    // Update last_time_checked
+    // It still needs to be updated even if the tab isnt focused
+    // to prevent a large amount of time being added at once later
     const current_time = Date.now();
     const time_since = current_time - last_time_checked;
     last_time_checked = current_time;
+    last_day = new Date().toString();
+    
+    // Check if the tab is focused
+    // Prevents double counting and only counts one page at a time
+    if (!document.hasFocus()) {return;}
+    
+    // Check if it's a new day
+    // If the day changes the counter needs to be reset
+    if (last_day != new Date().toString()) {
+        browser.storage.local.set({"limit_used" : 0})
+        return;
+    }
     let limit_used = browser.storage.local.get("limit_used")
     limit_used.then((item) => {
         const new_limit_used = {"limit_used" : item.limit_used + time_since};
@@ -40,19 +61,26 @@ async function check_over_limit() {
 
     if (ignore === true) return;
     
-    if (limit_used.limit_used >= limit.limit) {
+    if (Date.now() - limit_extension < 300000) return;
+
+    if (limit_used.limit_used >= limit.limit && !popup_visible) {
         append_popup_window();
+        popup_visible = true;
     }
 }
 
 function continue_button_handler() {
-    
+    limit_extension = Date.now(); // Sets 5 minute timer
+    const element = document.getElementById("limit-popup");
+    element.remove();
+    popup_visible = false;
 }
 
 function ignore_button_handler() {
-    ignore = true;
+    ignore = true; // ignores requests to append the popup window
     const element = document.getElementById("limit-popup");
     element.remove();
+    popup_visible = false;
 }
 
 // Yuck I wish I could use react or smth else this is hideous
@@ -69,6 +97,7 @@ function append_popup_window() {
 
     const button_1 = document.createElement("button");
     button_1.textContent = "5 more minutes";
+    button_1.addEventListener("click", continue_button_handler);
     content.appendChild(button_1);
 
     const button_2 = document.createElement("button");
@@ -82,5 +111,5 @@ function append_popup_window() {
 }
 
 initialize_storage();
-setInterval(update_limit_used, 60000);
+setInterval(update_limit_used, 5000);
 setInterval(check_over_limit, 1000);
